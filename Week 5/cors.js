e1c378c58252b3ce3d93f56ca47dd61bc3d0952d
Akshay Oberoi <akshayoberoi@Akshays-Laptop.local>
2025-07-21 19:29:21 +0530

    f

--- Week 5/cors.js
+++ Week 5/cors.js
@@ -6,6 +6,12 @@ app.use(cors({
 //cors comes into picture only when we are sending request fdrom a different domain ove rthe internet or more specificaaly acorees different doimains it will not be blocked across postman as it is localhost strill when i make it a service to 
 //deploy it will be blocked 
 
+//if rontend and vbackend are hosted on the ame domain then ther eis no use of the cors middleware
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/index.html")
+})
+
 app.use(express.json())
 app.post("sum",(req,res)=>{
     const a=parseInt(req.body.a)
