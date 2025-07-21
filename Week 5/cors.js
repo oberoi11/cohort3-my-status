583fecf39e7ff27e07614ffa32b6fed820fed479
Akshay Oberoi <akshayoberoi@Akshays-Laptop.local>
2025-07-21 19:26:56 +0530

    f

---
+++ Week 5/cors.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const app=express()
+app.use(cors({
+    domains:["url1","url2","url2"]
+}))
+//cors comes into picture only when we are sending request fdrom a different domain ove rthe internet or more specificaaly acorees different doimains it will not be blocked across postman as it is localhost strill when i make it a service to 
+//deploy it will be blocked 
+
+app.use(express.json())
+app.post("sum",(req,res)=>{
+    const a=parseInt(req.body.a)
+    const b=parseInt(req.body.b)
+    res,json({
+        ans:a+b
+    })
+})
+app.listen(3000)
