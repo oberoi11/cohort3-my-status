b97a49740d925e14fe9de0bdeb77d455b4228f7f
Akshay Oberoi <akshayoberoi@Akshays-Laptop.local>
2025-07-21 15:15:41 +0530

    Added rate limiter

--- Week 4/middleware.js
+++ Week 4/middleware.js
@@ -1,11 +1,27 @@
 const express=require("express");
 const app=express()
 let gc=0;
+let numberofrequests={}
+//something like this for keeping track of how many requests per persoon in a single second
+setInterval(()=>{
+    numberofrequests={}
+},1000)
+//request count
 app.use(function(req,res,next)){
+    //basic rate limiter
+    if(numberofrequests[req.headers["id"]]){
+        numberofrequests[req.headers["id"]]++;
+        if(numberofrequests[req.headers["id"]]>5){
+            res.status(411).send("no entry")
+        }else{
+            next()
+        }
+    }
     gc++;
     next()
 }
 app.use("/get",(req,res)=>{
+    numberofrequests[req.headers.id]++;
     //logic
 })
 app.use("/post",(req,res)=>{
@@ -15,4 +31,8 @@ app.use("/delete",(req,res)=>{
     //logic
 })
 //this will save ou server and help us keep track of the load on the server
+
+
+//rate limiter
+//only 5 requets per second
 app.listen(3000)
