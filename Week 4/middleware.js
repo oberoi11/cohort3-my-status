c54738aa74759933265663cf8500bff1d3c7863c
Akshay Oberoi <akshayoberoi@Akshays-Laptop.local>
2025-07-21 15:08:16 +0530

    first

--- Week 4/middleware.js
+++ Week 4/middleware.js
@@ -1,2 +1,18 @@
 const express=require("express");
 const app=express()
+let gc=0;
+app.use(function(req,res,next)){
+    gc++;
+    next()
+}
+app.use("/get",(req,res)=>{
+    //logic
+})
+app.use("/post",(req,res)=>{
+    //logic
+})
+app.use("/delete",(req,res)=>{
+    //logic
+})
+//this will save ou server and help us keep track of the load on the server
+app.listen(3000)
